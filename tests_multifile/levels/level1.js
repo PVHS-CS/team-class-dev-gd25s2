f3bb0e17fbee046acc6ce4197cb7557eb6680f60
Mr. Gardiner <matthew.gardiner@washk12.org>
2025-05-14 11:38:54 -0600

    split level 1

--- tests_multifile/levels/level1.js
+++ tests_multifile/levels/level1.js
@@ -1,48 +1,18 @@
 function createLevel1() {
     const map = [
-        "wwwwwwwwww",
-        "w        w",
-        "w        w",
-        "w        w",
-        "w        w",
-        "w        w",
-        "w        w",
-        "w        w",
-        "w        w",
-        "wwwwwwwwww"
+        "==========",
+        "=        =",
+        "=        =",
+        "=        =",
+        "=        =",
+        "=        =",
+        "=        =",
+        "=        =",
+        "=        =",
+        "=========="
     ];
     return addLevel(map, levelConfig);
 }
 
 // Register the level
 registerLevel("level1", createLevel1);
-
-function createLevel1(k) {
-    // Add some platforms
-    k.add([
-        k.rect(120, 20),
-        k.pos(40, 100),
-        k.area(),
-        k.body({ isStatic: true }),
-        k.color(0, 0, 255),
-    ]);
-
-    k.add([
-        k.rect(120, 20),
-        k.pos(200, 100),
-        k.area(),
-        k.body({ isStatic: true }),
-        k.color(0, 0, 255),
-    ]);
-
-    // Add some collectibles
-    for (let i = 0; i < 3; i++) {
-        k.add([
-            k.circle(8),
-            k.pos(k.rand(40, 280), k.rand(40, 80)),
-            k.area(),
-            k.color(0, 255, 0),
-            "bean",
-        ]);
-    }
-} 
